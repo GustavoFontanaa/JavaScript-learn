89cc3722a08bcbb06bbe4f8de60208fe9c9f5c19
Gustavo <gustavoasf77@gmail.com>
2022-02-13 15:23:58 -0300

    :tada: Create CardGame cmponent

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+    return `
+    <article class="card-game">
+        <img src="images/yugioh-image.png" alt="Logo Yugioh">
+    </article>
+    `;
+}
+
+export default CardGame;
